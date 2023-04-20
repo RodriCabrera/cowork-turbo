6ae395f9e08a23d7999da9414c10c5d0417be54a
Martin Soler <martinsoler_91@outlook.com>
2023-04-20 19:25:07 -0300

    boceto banner de landing page, imagen y texto a elegir

--- apps/front/pages/index.tsx
+++ apps/front/pages/index.tsx
@@ -1,8 +1,6 @@
-import Item from '@/components/Item'
-import { Button } from 'ui'
 import { TestInterface } from 'types'
 import NavBar from '../components/NavBar'
-
+import { CompanyBanner } from '@/components/CompanyBanner'
 export default function Web() {
   const something: TestInterface = { test: 'as' }
   console.log(something)
@@ -10,9 +8,7 @@ export default function Web() {
   return (
     <div>
       <NavBar />
-      <h1 className="px-2 py-4 text-4xl md:text-5xl">Web</h1>
-      <Button />
-      <Item />
+<CompanyBanner/>
     </div>
   )
 }
