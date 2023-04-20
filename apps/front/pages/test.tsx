0dc0303c526e560fac5a160bfc69b6fd95000503
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-20 21:35:11 +0200

    add test route

---
+++ apps/front/pages/test.tsx
@@ -0,0 +1,16 @@
+import Item from '@/components/Item'
+import { Button } from 'ui'
+
+import NavBar from '../components/NavBar'
+
+export default function Test() {
+  return (
+    <div className="bg-diarpu">
+      <NavBar />
+      <h1 className="px-2 py-4 text-4xl md:text-5xl">Test Page</h1>
+
+      <Button />
+      <Item />
+    </div>
+  )
+}
