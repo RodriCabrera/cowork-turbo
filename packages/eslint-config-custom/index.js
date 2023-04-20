f9338c59f3e45192bb2299d5b8d943bf5e90134c
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-20 21:53:15 +0200

    add eslint plugin tailwind

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -4,7 +4,12 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['plugin:react/recommended', 'standard', 'next'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:tailwindcss/recommended',
+    'standard',
+    'next'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
