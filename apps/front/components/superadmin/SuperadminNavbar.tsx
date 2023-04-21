8d8ac79b740c11827fc24ea91ebce349965fe75c
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-22 00:29:35 +0200

    refactor

---
+++ apps/front/components/superadmin/SuperadminNavbar.tsx
@@ -0,0 +1,3 @@
+export const SuperadminNavbar = () => {
+  return <div className="h-16 bg-gray-800">Navbar</div>
+}
--- apps/front/pages/index.tsx
+++ apps/front/pages/index.tsx
@@ -1,14 +1,12 @@
-import { TestInterface } from 'types'
-import NavBar from '../components/NavBar'
-import { CompanyBanner } from '@/components/CompanyBanner'
-export default function Web() {
-  const something: TestInterface = { test: 'as' }
-  console.log(something)
+import { Button } from 'ui'
 
+const Landing = () => {
   return (
-    <div>
-      <NavBar />
-      <CompanyBanner />
-    </div>
+    <>
+      <h1 className="px-2 py-4 text-4xl md:text-5xl">Web</h1>
+      <Button />
+    </>
   )
 }
+
+export default Landing
