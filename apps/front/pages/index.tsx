b511bd4298d6d27a011441a52d832e990d55e8df
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-22 00:32:01 +0200

    missing banner

--- apps/front/pages/index.tsx
+++ apps/front/pages/index.tsx
@@ -1,9 +1,11 @@
+import { CompanyBanner } from '@/components/CompanyBanner'
 import { Button } from 'ui'
 
 const Landing = () => {
   return (
     <>
       <h1 className="px-2 py-4 text-4xl md:text-5xl">Web</h1>
+      <CompanyBanner />
       <Button />
     </>
   )
