8d902e13286534e98bfaa6b10364b7f96d57c23b
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-23 01:41:30 +0200

    test: rename test

--- apps/front/tests/home.test.tsx
+++ apps/front/tests/home.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 import '@testing-library/jest-dom'
 
-describe('Home', () => {
+describe('User Home', () => {
   it('renders a heading', () => {
     render(<Home />)
 
