aedf0c5b7dcfac931780929ade7223758808d0d8
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-23 19:16:40 +0200

    add fonts

---
+++ apps/front/styles/fonts.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line camelcase
+import { Bungee_Hairline, Raleway } from 'next/font/google'
+
+export const bungee = Bungee_Hairline({ weight: '400', subsets: ['latin'] })
+
+export const raleway = Raleway({ subsets: ['latin'] })
