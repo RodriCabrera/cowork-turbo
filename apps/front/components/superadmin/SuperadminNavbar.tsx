776f470471193266698b8cf9a833fe7effcbf0a5
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-23 19:16:36 +0200

    style: add bunge font to superadmin nav

--- apps/front/components/superadmin/SuperadminNavbar.tsx
+++ apps/front/components/superadmin/SuperadminNavbar.tsx
@@ -1,7 +1,9 @@
-export const SuperadminNavbar = () => {
-  return (
-    <div className="h-16  border-b-2 pl-10 pt-3 text-3xl font-bold ">
-      BaseBloom
-    </div>
-  )
-}
+import { bungee } from '@/styles/fonts'
+
+export const SuperadminNavbar = () => (
+  <div
+    className={`${bungee.className} h-16 border-b-2 pl-10 pt-3 text-3xl font-bold`}
+  >
+    BaseBloom
+  </div>
+)
