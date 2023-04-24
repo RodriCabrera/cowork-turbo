b4833e347d5e5272107507aff26dad0edb200c3d
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-24 21:48:56 +0200

    style: move tailwind classes back to inline

--- apps/front/components/superadmin/SuperadminLogin.tsx
+++ apps/front/components/superadmin/SuperadminLogin.tsx
@@ -27,19 +27,21 @@ export const SuperadminLogin = () => {
       },
       body: JSON.stringify({ email: enteredEmail })
     })
-      .then(res => {
+      .then((res) => {
         if (res.status === 200) {
           setQueryStatus(STATUS.sent)
           // TODO: Change form to display just the OK message
           setBtnMessage('Email sent, check your inbox')
         }
         if (res.status === 401) throw new Error('Email not found')
-      }).catch(err => {
+      })
+      .catch((err) => {
         setQueryStatus(STATUS.error)
         if (err instanceof Error) {
           setBtnMessage(err.message)
         } else setBtnMessage('Could not send mail')
-      }).finally(() => {
+      })
+      .finally(() => {
         setTimeout(() => {
           setQueryStatus(STATUS.waiting)
           setBtnMessage('Sign in')
@@ -50,26 +52,17 @@ export const SuperadminLogin = () => {
   const emailInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
     setEnteredEmail(e.target.value)
 
-  const styles = {
-    section:
-      'mx-auto flex flex-col items-center justify-center bg-gray-50 px-6 py-8 h-[calc(100vh-62px)] lg:py-0',
-    card: 'w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 ',
-    formContainer: 'space-y-4 p-6 sm:p-8 md:space-y-6',
-    form: 'space-y-4 md:space-y-6',
-    label: 'mb-2 block text-xl ',
-    input:
-      'block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm',
-    signInButton:
-      'w-full rounded-lg bg-green-100/75 px-5 py-2.5 text-center text-base font-light text-black focus:outline-none focus:ring-4 hover:bg-green-200/75'
-  }
-
   return (
-    <section className={styles.section}>
-      <div className={styles.card}>
-        <div className={styles.formContainer}>
-          <form className={styles.form} action="#" onSubmit={formSubmitHandler}>
+    <section className="mx-auto flex h-[calc(100vh-62px)] flex-col items-center justify-center bg-gray-50 px-6 py-8 lg:py-0">
+      <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
+        <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+          <form
+            className="space-y-4 md:space-y-6"
+            action="#"
+            onSubmit={formSubmitHandler}
+          >
             <div>
-              <label htmlFor="email" className={styles.label}>
+              <label htmlFor="email" className="mb-2 block text-xl">
                 Email
               </label>
               <input
@@ -78,12 +71,15 @@ export const SuperadminLogin = () => {
                 onChange={emailInputChangeHandler}
                 name="email"
                 id="email"
-                className={styles.input}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm"
                 required
                 disabled={queryStatus !== STATUS.waiting}
               />
             </div>
-            <button type="submit" className={styles.signInButton}>
+            <button
+              type="submit"
+              className="w-full rounded-lg bg-green-100/75 px-5 py-2.5 text-center text-base font-light text-black hover:bg-green-200/75 focus:outline-none focus:ring-4"
+            >
               {btnMessage}
             </button>
           </form>
