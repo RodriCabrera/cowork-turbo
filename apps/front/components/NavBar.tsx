6e790c667cc0e120eaa0b2bbd8e807d83801d6c8
Martin Soler <martinsoler_91@outlook.com>
2023-04-24 09:43:14 -0300

    fix: NavBar layout and styling

--- apps/front/components/NavBar.tsx
+++ apps/front/components/NavBar.tsx
@@ -1,9 +1,6 @@
 import { Fragment } from 'react'
-import Image from 'next/image'
 import Link from 'next/link'
-
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Quienes Somos', href: 'about', current: false },
@@ -14,70 +11,53 @@ const classNames = (...classes: string[]) => classes.filter(Boolean).join(' ')
 
 export function NavBar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="border-b-2 border-gray-300">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="mx-1 max-w-full px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-24 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
+              <div className="flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex shrink-0 items-center">
-                  <Link href={'/'}>
-                    <Image
-                      className="block h-8 w-auto lg:hidden"
-                      src="https://banner2.cleanpng.com/20180612/iix/kisspng-insurgency-logo-new-world-interactive-desktop-wall-5b1f5cf11a5c64.051287471528782065108.jpg"
-                      alt="Your Company"
-                      width={300}
-                      height={300}
-                    />
-                    <Image
-                      height={300}
-                      width={300}
-                      className="hidden h-8 w-auto lg:block"
-                      src="https://banner2.cleanpng.com/20180612/iix/kisspng-insurgency-logo-new-world-interactive-desktop-wall-5b1f5cf11a5c64.051287471528782065108.jpg"
-                      alt="Your Company"
-                    />
+                  <Link href={'/'} className="text-4xl font-bold text-black">
+                    BaseBloom
                   </Link>
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <Link
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </Link>
-                    ))}
-                  </div>
-                </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute inset-y-0 right-0 flex items-center rounded-3xl border-2 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
+                <Menu as="div" className="px-1">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className=" flex items-center space-x-2 rounded-full p-2 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-4 focus:ring-offset-gray-200">
                       <span className="sr-only">Open user menu</span>
-                      <span className="px-5 py-2 text-base text-gray-300 hover:text-gray-100">
-                        Acceso
-                      </span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="h-6 w-6 bg-white text-gray-700"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                        />
+                      </svg>
+                      <div className="relative h-7 w-7 overflow-hidden rounded-full bg-white ">
+                        <svg
+                          className="absolute -left-1 h-9 w-9 bg-gray-400 text-white"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                            clip-rule="evenodd"
+                          ></path>
+                        </svg>
+                      </div>
                     </Menu.Button>
                   </div>
                   <Transition
@@ -91,7 +71,7 @@ export function NavBar() {
                   >
                     {/* TODO: Fix this ring-opacity issue: */}
                     {/* eslint-disable-next-line tailwindcss/migration-from-tailwind-2 */}
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <Link
@@ -101,7 +81,20 @@ export function NavBar() {
                               'block px-4 py-2 text-sm text-gray-700'
                             )}
                           >
-                            Empresa/Freelancer
+                            Registrate como empresa
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href=""
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block border-b-2 px-4 py-2 text-sm text-gray-700'
+                            )}
+                          >
+                            Registrate como freelancer
                           </Link>
                         )}
                       </Menu.Item>
@@ -114,7 +107,20 @@ export function NavBar() {
                               'block px-4 py-2 text-sm text-gray-700'
                             )}
                           >
-                            Coworker
+                            Iniciar sesion
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href=""
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm text-gray-700'
+                            )}
+                          >
+                            Ayuda
                           </Link>
                         )}
                       </Menu.Item>
