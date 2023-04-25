3774299706fac87a2d696c6811626188d9436b3e
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-25 17:24:56 +0200

    feat: if superadmin is logged in, redirect from login to dashboard

--- apps/front/components/superadmin/AuthenticatedNavbar.tsx
+++ apps/front/components/superadmin/AuthenticatedNavbar.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link'
-import React from 'react'
 
 import { PropsWithUser } from 'types'
 
 export const AuthenticatedNavbar = ({ user }: PropsWithUser) => {
   return (
     <div className="flex gap-10">
-      <p>{user?.name}</p>
       <Link href="/api/superadmin/logout">Logout</Link>
     </div>
   )
--- apps/front/pages/superadmin/index.tsx
+++ apps/front/pages/superadmin/index.tsx
@@ -2,8 +2,26 @@ import React, { ReactElement } from 'react'
 
 import { SuperadminLayout } from '@/components/superadmin/SuperadminLayout'
 import { SuperadminLogin } from '@/components/superadmin/SuperadminLogin'
+import { withSessionSsr } from '@/lib/withSession'
 
 export const Login = () => <SuperadminLogin />
+
+export const getServerSideProps = withSessionSsr(async ({ req }) => {
+  const { user } = req.session
+
+  if (user) {
+    return {
+      redirect: {
+        destination: '/superadmin/dashboard',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+})
 
 Login.getLayout = function getLayout(page: ReactElement) {
   return <SuperadminLayout>{page}</SuperadminLayout>
