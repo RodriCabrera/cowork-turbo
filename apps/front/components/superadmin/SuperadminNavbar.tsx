f27ce7c31f9f7cb4363c345fa3b9ce85fb9aefd7
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-25 18:32:41 +0200

    style(superadmin nav): move bungee font to logo

--- apps/front/components/superadmin/SuperadminNavbar.tsx
+++ apps/front/components/superadmin/SuperadminNavbar.tsx
@@ -5,9 +5,11 @@ import { PropsWithUser } from '@/../../packages/types'
 export const SuperadminNavbar = ({ user }: PropsWithUser) => {
   return (
     <nav
-      className={`${bungee.className} flex h-16 justify-between border-b-2 pl-10 pt-3 text-3xl font-bold`}
+      className={
+        'flex h-16 justify-between border-b-2 pl-10 pt-3 text-3xl font-bold'
+      }
     >
-      BaseBloom
+      <header className={`${bungee.className}`}>BaseBloom</header>
       {user && <AuthenticatedNavbar user={user} />}
     </nav>
   )
