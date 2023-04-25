f66cb70937de654ad252977a547e69691d5b591e
Martin Soler <martinsoler_91@outlook.com>
2023-04-25 11:24:14 -0300

    code reorganized and styling changed

--- apps/front/components/NavBar.tsx
+++ apps/front/components/NavBar.tsx
@@ -4,6 +4,13 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { bungee } from '@/styles/fonts'
 import { Bars3Icon, UserIcon } from '@heroicons/react/24/solid'
 
+const dropdownOptions = [
+  { name: 'Registrate como empresa', href: '#', current: false },
+  { name: 'Registrate como freelancer', href: '#', current: false },
+  { name: 'Iniciar sesion', href: '#', current: false },
+  { name: 'Ayuda', href: '#', current: false }
+]
+
 const classNames = (...classes: string[]) => classes.filter(Boolean).join(' ')
 
 export function NavBar() {
@@ -18,20 +25,20 @@ export function NavBar() {
                 <div className="flex shrink-0 items-center">
                   <Link
                     href="/"
-                    className={`${bungee.className} h-16 pl-5 pt-4 text-2xl font-bold md:pl-10 md:text-4xl`}
+                    className={`${bungee.className} h-16 pl-2 pt-4 text-3xl font-bold md:pl-10 md:text-4xl`}
                   >
                     BaseBloom
                   </Link>
                 </div>
               </div>
-              <div className=" inset-y-0 right-0 mr-5 flex items-center rounded-3xl border-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className=" inset-y-0 right-0 mr-2  flex items-center rounded-3xl border-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* Profile dropdown */}
-                <Menu as="div" className="px-2 sm:px-0">
+                <Menu as="div" className="px-1 sm:px-2">
                   <div>
-                    <Menu.Button className="flex items-center space-x-2 rounded-full p-0 text-sm focus:outline-none focus:ring-4 focus:ring-white focus:ring-offset-4 focus:ring-offset-gray-200 sm:p-1">
+                    <Menu.Button className="flex items-center space-x-2 rounded-full p-1 text-sm focus:outline-none focus:ring-4 focus:ring-white focus:ring-offset-4 focus:ring-offset-gray-700 ">
                       <span className="sr-only">Open user menu</span>
-                      <Bars3Icon className="h-10 w-6 bg-white text-gray-700" />
-                      <div className="relative h-7 w-7 overflow-hidden rounded-full bg-white ">
+                      <Bars3Icon className="h-6 w-6 bg-white text-gray-700 sm:h-10" />
+                      <div className="relative h-7 w-7 overflow-hidden rounded-full bg-white">
                         <UserIcon className="absolute -left-1 h-9 w-9 bg-gray-400 text-white" />
                       </div>
                     </Menu.Button>
@@ -48,58 +55,21 @@ export function NavBar() {
                     {/* TODO: Fix this ring-opacity issue: */}
                     {/* eslint-disable-next-line tailwindcss/migration-from-tailwind-2 */}
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            href="/test"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Registrate como empresa
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            href=""
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block border-b-2 px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Registrate como freelancer
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            href=""
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Iniciar sesion
-                          </Link>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <Link
-                            href=""
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Ayuda
-                          </Link>
-                        )}
-                      </Menu.Item>
+                      {dropdownOptions.map((option, index) => (
+                        <Menu.Item key={index}>
+                          {({ active }) => (
+                            <Link
+                              href={option.href}
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-gray-700'
+                              )}
+                            >
+                              {option.name}
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      ))}
                     </Menu.Items>
                   </Transition>
                 </Menu>
