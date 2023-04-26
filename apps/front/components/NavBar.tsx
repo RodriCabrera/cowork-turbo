eb4caa4a4bc07ccade7d321fccd238c300c54cf8
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-26 21:23:13 +0200

    style: adjust nav border

--- apps/front/components/NavBar.tsx
+++ apps/front/components/NavBar.tsx
@@ -15,7 +15,7 @@ const classNames = (...classes: string[]) => classes.filter(Boolean).join(' ')
 
 export function NavBar() {
   return (
-    <nav className="border-b-2 border-gray-300">
+    <nav className="border-b-2 bg-white">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
