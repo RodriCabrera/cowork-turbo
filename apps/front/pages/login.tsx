ea76aa6864e3ab504d042be55692e9102bf95fe2
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-29 01:20:41 +0200

    feat(front/UA): add login form to login page

--- apps/front/pages/login.tsx
+++ apps/front/pages/login.tsx
@@ -1,5 +1,11 @@
-export const Login = () => {
-  return <div>LoginPage</div>
+import { Login } from '@/components/Login'
+
+export const UserLogin = () => {
+  return (
+    <div className="flex h-[calc(100vh-64px)] w-screen items-center justify-center bg-gray-50">
+      <Login endpoint="login" />
+    </div>
+  )
 }
 
-export default Login
+export default UserLogin
