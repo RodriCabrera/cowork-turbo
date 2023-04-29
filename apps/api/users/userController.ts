3802ecb1eac57d051b0b67f84f12586a24d2a0b7
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-04-29 17:42:48 +0200

    feat: add user auth & login endpoints
    based on the superadmin login.

--- apps/api/users/userController.ts
+++ apps/api/users/userController.ts
@@ -1,4 +1,13 @@
-import { Get, Path, Route, Tags } from 'tsoa'
+import {
+  BodyProp,
+  Get,
+  Path,
+  Post,
+  Response,
+  Route,
+  SuccessResponse,
+  Tags
+} from 'tsoa'
 import UserService from './userService'
 import { User } from '@prisma/client'
 
@@ -13,5 +22,27 @@ export default class UserController {
   @Get('/{id}')
   async getUser(@Path() id: string): Promise<User> {
     return UserService.fetchById(id)
+  }
+
+  /**
+   *  Send login email to the address given if the user exists
+   * @returns OK or 401
+   */
+  @Response('401', 'Unauthorized')
+  @SuccessResponse('200', 'Email sent')
+  @Post('/login')
+  static async requestAuth(@BodyProp() email: string) {
+    return UserService.sendAuthEmail(email)
+  }
+
+  /**
+   * Receives user's ID & token and checks if those match the database.
+   * @returns OK or 401
+   */
+  @Response('401', 'Unauthorized')
+  @SuccessResponse('200', 'Authorized')
+  @Post('/auth')
+  static async auth(@BodyProp() id: string, @BodyProp() token: string) {
+    return UserService.checkAuthorization(id, token)
   }
 }
--- apps/api/users/userService.ts
+++ apps/api/users/userService.ts
@@ -1,4 +1,11 @@
 import { PrismaClient } from '@prisma/client'
+import { compare, genSalt, hash } from 'bcryptjs'
+import CustomError, { ERROR_CODES } from '../errors/customError'
+import PrismaErrors from '../errors/prismaErrors'
+import { v4 as uuid } from 'uuid'
+import jwt from 'jsonwebtoken'
+import MailService from '../mail/mailService'
+import { loginTemplate } from '../mail/templates'
 
 export default class UserService {
   private static _client = new PrismaClient()
@@ -25,6 +32,76 @@ export default class UserService {
       return response
     } catch (err) {
       console.error(err)
+    } finally {
+      this._client.$disconnect()
+    }
+  }
+
+  static async sendAuthEmail(email: string): Promise<boolean> {
+    this._client.$connect()
+    try {
+      const salt = await genSalt(10)
+      const token = uuid()
+      const cryptedToken = await hash(token, salt)
+      const user = await this._client.user.update({
+        where: {
+          mail: email
+        },
+        data: {
+          token: cryptedToken
+        }
+      })
+      const userJWT = jwt.sign(
+        {
+          ...user,
+          token
+        },
+        process.env.SESSION_SECRET
+      )
+      const mailService = MailService.getInstance()
+      await mailService.sendMail({
+        from: 'noreply@localhost.com',
+        to: user.mail,
+        subject: 'Login to your account',
+        html: loginTemplate(
+          `${user.firstName} ${user.lastName}`,
+          userJWT,
+          user.rol
+        )
+      })
+      return true
+    } catch (err) {
+      PrismaErrors.parseError(err, 'User')
+      return false
+    } finally {
+      this._client.$disconnect()
+    }
+  }
+
+  static async checkAuthorization(id: string, token: string): Promise<boolean> {
+    this._client.$connect()
+    try {
+      const user = await this._client.user.findUniqueOrThrow({
+        where: { id }
+      })
+      if (user.token && (await compare(token, user.token))) {
+        await this._client.user.update({
+          where: {
+            id: user.id
+          },
+          data: {
+            token: null
+          }
+        })
+        return true
+      }
+      throw new CustomError(
+        'Invalid token',
+        401,
+        ERROR_CODES.TokenExpiredOrInvalid
+      )
+    } catch (err) {
+      throw PrismaErrors.parseError(err, 'Superadmin')
     } finally {
       this._client.$disconnect()
     }
