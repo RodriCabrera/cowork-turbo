f21338833f2b6957397d41e2c47ee328c75f966e
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-05-01 19:51:20 -0300

    docs: add security lock to protected routes

--- apps/api/coworks/coworkController.ts
+++ apps/api/coworks/coworkController.ts
@@ -8,12 +8,13 @@ import {
   Delete,
   Body,
   Response,
-  Request as tsoaReq
+  Request,
+  Security
 } from 'tsoa'
 import CoworkService from './coworkService'
 import { CreateCoworkInput, EditCoworkInput } from './coworkTypes'
 import CustomError from '../errors/customError'
-import { Request } from 'express'
+import express from 'express'
 
 @Route('coworks')
 @Tags('Coworks')
@@ -37,6 +38,8 @@ export default class CoworkController {
    */
   @Response<CustomError>(404, 'Cowork not found')
   @Response<CustomError>(406, 'Input data not valid')
+  @Response<CustomError>(401, 'Unauthorized')
+  @Security('')
   @Put('/{id}')
   static async edit(@Path() id: string, @Body() data: EditCoworkInput) {
     return CoworkService.edit(id, data)
@@ -48,15 +51,24 @@ export default class CoworkController {
    * @returns Cowork
    */
   @Response<CustomError>(406, 'Input data not valid')
+  @Response<CustomError>(401, 'Unauthorized')
+  @Security('')
   @Post('/')
   static async create(
     @Body() data: CreateCoworkInput,
-    @tsoaReq() req: Request
+    @Request() req: express.Request
   ) {
     return CoworkService.createCowork(data, req.user?.id || 'anon')
   }
 
+  /**
+   *
+   * @param id
+   * @returns
+   */
   @Response<CustomError>(404, 'Cowork not found')
+  @Response<CustomError>(401, 'Unauthorized')
+  @Security('')
   @Delete('/{id}')
   static async remove(@Path() id: string) {
     return CoworkService.delete(id)
