68e6610b618e1e3906208bf0126437b4adf2ed89
Rodrigo <65794827+RodriCabrera@users.noreply.github.com>
2023-05-04 19:06:11 +0200

    refactor: move only front types into front app (#45)

--- apps/front/pages/superadmin/coworks/new.tsx
+++ apps/front/pages/superadmin/coworks/new.tsx
@@ -2,10 +2,9 @@ import { ReactElement } from 'react'
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
 
-import { PropsWithSuperadmin } from 'types'
-
 import { SuperadminLayout } from '@/components/superadmin/SuperadminLayout'
 import { getSuperAdminData } from '@/lib/superadmin'
+import { PropsWithSuperadmin } from '@/types/superadmin'
 
 export const NewCoworkPage = ({ superadmin }: PropsWithSuperadmin) => {
   const router = useRouter()
