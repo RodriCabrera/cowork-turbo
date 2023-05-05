9e161f3b33c2fc38dfe3cc4a1d357bf275e12854
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-05-05 15:17:49 -0300

    feat: add trim to cowork create validator to avoid empty whitespace inputs

--- apps/front/validators/newCoworkValidation.ts
+++ apps/front/validators/newCoworkValidation.ts
@@ -4,9 +4,9 @@ export const createCoworkSchema = z.object({
   email: z
     .string({ required_error: 'Email is required' })
     .email({ message: 'Must be a valid email' }),
-  name: z.string().min(1, { message: 'Name is required' }),
-  phone: z.string().min(1, { message: 'Phone is required' }),
-  description: z.string().min(1, { message: 'Description is required' }),
+  name: z.string().trim().min(1, { message: 'Name is required' }),
+  phone: z.string().trim().min(1, { message: 'Phone is required' }),
+  description: z.string().trim().min(1, { message: 'Description is required' }),
   status: z
     .enum(['ACTIVE', 'PAUSED', 'CLOSED'], {
       invalid_type_error: 'Status must be ACTIVE, PAUSED or CLOSED'
@@ -14,13 +14,19 @@ export const createCoworkSchema = z.object({
     .optional(),
   address: z.object(
     {
-      country: z.string().min(1, { message: 'Country is required' }),
-      city: z.string().min(1, { message: 'City is required' }),
-      streetName: z.string().min(1, { message: 'Street name is required' }),
-      number: z.string().min(1, { message: 'Street number is required' }),
-      floor: z.string().optional(),
-      apartment: z.string().optional(),
-      postalCode: z.string().optional()
+      country: z.string().trim().min(1, { message: 'Country is required' }),
+      city: z.string().trim().min(1, { message: 'City is required' }),
+      streetName: z
+        .string()
+        .trim()
+        .min(1, { message: 'Street name is required' }),
+      number: z
+        .string()
+        .trim()
+        .min(1, { message: 'Street number is required' }),
+      floor: z.string().trim().optional(),
+      apartment: z.string().trim().optional(),
+      postalCode: z.string().trim().optional()
     },
     { required_error: 'Address is required' }
   )
