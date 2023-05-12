c6d3d7e9df1f2a7b3ae207e2f5285e3a164ccab9
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-05-12 14:02:20 -0300

    refactor: update frontend types and params to adjust to changes in api response

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -51,5 +51,10 @@ export type PaginatedResponse<T> = {
 
 export type CoworkCreateReq = CreateCoworkInput
 export type CoworkEditReq = EditCoworkInput
-export type CoworkFullGetRes = CoworkFull
+export type SingleCoworkFullGetRes = CoworkFull
+export type CoworkFullGetRes = PaginatedResponse<CoworkFull[]>
 export type ErrorAllRes = ErrorInterface
+
+// Returns type of elements of typed Array -> ie: ArrayType<CoworkFull[]> = CoworkFull
+export type ArrayElement<ArrayType extends readonly unknown[]> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never
