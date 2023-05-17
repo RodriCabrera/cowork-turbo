898e81cafbfa5154348c761a913322226a2f4c04
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-05-17 12:36:14 +0200

    create pagination component

--- apps/front/components/superadmin/CoworksTable/index.tsx
+++ apps/front/components/superadmin/CoworksTable/index.tsx
@@ -8,6 +8,7 @@ import { ActionsCell } from './ActionsCell'
 import { Placeholder } from './Placeholder'
 import { useGetCoworks } from '@/hooks/useGetCoworks'
 import { COLORS_BY_STATUS } from './constants'
+import { Pagination } from '@/components/pagination'
 
 const { Cell, Body, Header, Row } = Table
 
@@ -155,33 +156,13 @@ export const CoworksTable = () => {
               </Body>
             </Table>
           </div>
-          <div className="flex justify-end pb-8 pt-0">
-            <div className="flex items-center gap-4 rounded-md bg-white p-2">
-              <div>Page size:</div>
-              <select onChange={(e) => handlePageSizeChange(+e.target.value)}>
-                <option value={10}>10</option>
-                <option value={20}>20</option>
-                <option value={50}>50</option>
-              </select>
-              <button
-                onClick={prevPage}
-                disabled={pageIndex === 1}
-                className="cursor-pointer rounded-md border-2 bg-gray-50/100 px-3 py-2 text-sm font-medium"
-              >
-                {'<'}
-              </button>
-              <p>
-                Page {pageIndex} of {totalPages}
-              </p>
-              <button
-                onClick={nextPage}
-                disabled={pageIndex === totalPages}
-                className="cursor-pointer rounded-md border-2 bg-gray-50/100 px-3 py-2 text-sm font-medium"
-              >
-                {'>'}
-              </button>
-            </div>
-          </div>
+          <Pagination
+            pageIndex={pageIndex}
+            totalPages={totalPages}
+            handlePageSizeChange={handlePageSizeChange}
+            prevPage={prevPage}
+            nextPage={nextPage}
+          />
         </div>
       </div>
     </div>
