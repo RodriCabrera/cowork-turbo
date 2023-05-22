73dd49951e5ea8afbea9a862287089fc4bcd1a8d
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-05-22 18:11:24 -0300

    fix: missing user auth route

--- apps/api/users/userRouter.ts
+++ apps/api/users/userRouter.ts
@@ -15,6 +15,15 @@ userRoutes.get('/:id', async (req: Request, res: Response) => {
   return res.send(response)
 })
 
+userRoutes.post('/auth', async (req, res, next) => {
+  try {
+    const response = await UserController.auth(req.body.id, req.body.token)
+    return res.send(response)
+  } catch (err) {
+    next(err)
+  }
+})
+
 userRoutes.post('/register/admin', async (req: Request, res: Response) => {
   const response = await UserController.registerAdmin(req.body)
   return res.send(response)
