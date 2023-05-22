cc85bb5a943844bcf2a32496900bed77de5f738f
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-05-22 16:36:57 +0200

    refactor/front(cowork table): make CellPlaceholder reusable for tables

---
+++ apps/front/modules/coworks/components/CoworksTable/CellPlaceholder.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Table } from 'ui'
+
+export const CellPlaceholder = ({ text }: { text: string }) => (
+  <Table.Row>
+    <Table.Cell>{text}</Table.Cell>
+  </Table.Row>
+)
