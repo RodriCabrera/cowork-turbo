cfc874f1ca6228fd1063910772669fa2a6d9262c
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-05-22 21:19:07 +0200

    front/style: remove freelancer form shadow

--- apps/front/modules/auth/components/RegisterForms/FreelancerForm/index.tsx
+++ apps/front/modules/auth/components/RegisterForms/FreelancerForm/index.tsx
@@ -36,7 +36,7 @@ export const FreelancerForm = () => {
 
   return (
     <section className="mx-auto flex  w-full flex-col items-center justify-center px-6 ">
-      <div className="w-full rounded-lg bg-white sm:max-w-md md:mt-0 lg:shadow xl:p-0">
+      <div className="w-full rounded-lg bg-white sm:max-w-md md:mt-0  xl:p-0">
         <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
           <form
             className="space-y-4 md:space-y-6"
