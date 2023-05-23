a80b86df321c98eff7ea701502aa05620edde4bc
Rodrigo <65794827+RodriCabrera@users.noreply.github.com>
2023-05-23 17:03:44 +0200

    [COW-25] front/feat(admin login): create api route (#73)
    front/feat(admin login): create api route

--- apps/front/additional.d.ts
+++ apps/front/additional.d.ts
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
-import { SuperAdminData } from 'types'
+import { SuperAdminData, AdminData } from 'types'
 import { Auth } from './modules/auth/types'
 
 declare module 'iron-session' {
   interface IronSessionData {
     superadmin?: SuperAdminData & Auth
+    admin?: AdminData & Auth
   }
 }
