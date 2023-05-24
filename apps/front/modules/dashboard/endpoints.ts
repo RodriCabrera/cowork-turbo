f814f73909e3ea4a2fc6edf17ff82d2481839c28
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-05-24 22:17:50 +0200

    addEmployees endpoint

--- apps/front/modules/dashboard/endpoints.ts
+++ apps/front/modules/dashboard/endpoints.ts
@@ -1,6 +1,21 @@
 import { AxiosInstance } from 'axios'
 
+import { CompanyGetOneRes, EmployeeAddReq } from 'types'
+
 export const getCompany = async (
   api: AxiosInstance,
   companyId: string | undefined
-) => await api.get(`/companies/${companyId}`)
+) => await api.get<CompanyGetOneRes>(`/companies/${companyId}`)
+
+export const addEmployees = async (
+  api: AxiosInstance,
+  companyId: string | undefined,
+  employees: EmployeeAddReq
+) =>
+  await api.post<
+    EmployeeAddReq,
+    {
+      sent: boolean
+      email: string
+    }[]
+  >(`/companies/employees?idCompany=${companyId}`, { employees })
