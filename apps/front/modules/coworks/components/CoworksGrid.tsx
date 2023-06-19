8e77232abf3c2a1fe8c3f7be9bd1611ad0c4180e
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-06-19 16:51:02 +0200

    adjust cowork grid

--- apps/front/modules/coworks/components/CoworksGrid.tsx
+++ apps/front/modules/coworks/components/CoworksGrid.tsx
@@ -1,41 +1,37 @@
 import React from 'react'
+
 import { CoworkCard } from './CoworkCard'
 import { useInfiniteCoworks } from '../hooks/useInfiniteCoworks'
 
 export const CoworksGrid = () => {
-  const {
-    data,
-    error,
-    hasNextPage,
-    fetchNextPage,
-    isFetching,
-    isFetchingNextPage,
-    status
-  } = useInfiniteCoworks()
-  console.log('has next page', hasNextPage)
+  const { data, hasNextPage, fetchNextPage, isFetching, isFetchingNextPage } =
+    useInfiniteCoworks()
 
   return (
-    <div className="flex w-2/3 flex-wrap gap-4 pb-12">
-      {data &&
-        data?.pages.map((page, index) => (
-          <React.Fragment key={index}>
-            {page.results?.map((cowork) => (
-              <CoworkCard key={cowork.id} cowork={cowork} />
-            ))}
-          </React.Fragment>
-        ))}
-      {/* TODO: Check the notifications */}
-      {hasNextPage && !isFetchingNextPage ? (
+    <>
+      <div className="flex w-2/3 flex-wrap gap-4 pb-12">
+        {data &&
+          data?.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.results?.map((cowork) => (
+                <CoworkCard key={cowork.id} cowork={cowork} />
+              ))}
+            </React.Fragment>
+          ))}
+      </div>
+
+      {hasNextPage && !isFetchingNextPage && !isFetching && (
         <button
-          className="rounded-md bg-yellow-100 px-3 py-2"
+          className="h-12 rounded-md bg-yellow-100 px-3 py-2"
           onClick={() => fetchNextPage()}
         >
-          fetch more
+          Fetch more
         </button>
-      ) : (
-        <p>Loading</p>
       )}
-      {!isFetchingNextPage && !hasNextPage && <p>No more coworks</p>}
-    </div>
+      {(isFetchingNextPage || isFetching) && <p>Loading...</p>}
+      {!isFetchingNextPage && !isFetching && !hasNextPage && (
+        <p>No more coworks</p>
+      )}
+    </>
   )
 }
