ab568c52db078b8b5994b002d2fbc12d8308a801
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-07-06 21:28:53 +0200

    feat: update about page content

--- apps/front/modules/landing/banners/AboutBanner.tsx
+++ apps/front/modules/landing/banners/AboutBanner.tsx
@@ -2,16 +2,16 @@ export const AboutBanner = () => {
   return (
     <section className="max-w-7xl px-4 pt-6 sm:w-auto md:pt-10">
       <header className="m-1 text-6xl text-gray-900 sm:text-7xl lg:text-8xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing
+        Our objective is to keep it simple
         <span className="text-green-300">.</span>
       </header>
       <div className="mt-11 flex justify-end">
-        <div className="mx-2 flex w-full flex-wrap gap-14 text-lg leading-8 text-gray-600 sm:flex sm:w-1/3 md:text-xl">
-          <article>
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
-            Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
-            In mi viverra elit nunc.
-          </article>
+        <div className="mx-2 flex w-full flex-wrap text-2xl text-gray-600 sm:flex  md:text-4xl">
+          <p>
+            Wether you&apos;re an employee, or the manager of a cowork or
+            company,
+          </p>
+          <p className="text-green-300">leave the arrangements to us.</p>
         </div>
       </div>
     </section>
