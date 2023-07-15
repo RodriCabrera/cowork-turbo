335eca6eb824eb08016942e30666f16b67b35491
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-07-15 18:34:00 -0300

    refactor(front): add wildcard to allowed images hostnames

--- apps/front/next.config.js
+++ apps/front/next.config.js
@@ -5,25 +5,7 @@ module.exports = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'banner2.cleanpng.com',
-        port: '',
-        pathname: '/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-        port: '',
-        pathname: '/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'lamaquinita.co',
-        port: '',
-        pathname: '/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.it',
+        hostname: '*',
         port: '',
         pathname: '/**'
       }
