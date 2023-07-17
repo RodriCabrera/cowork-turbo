c1e8a581146220c4562ef8659465a6da261fa908
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-07-17 12:39:02 -0300

    style(front): cowork details minimal style

--- apps/front/modules/coworks/components/CoworkDetails/index.tsx
+++ apps/front/modules/coworks/components/CoworkDetails/index.tsx
@@ -1,28 +1,58 @@
+import Image from 'next/image'
+
 import { useFetchCoworkDetails } from '../../hooks/useFetchCoworkDetails'
 
 export const CoworkDetails = ({ coworkId }: { coworkId: string }) => {
-  const { coworkDetails, isFetched } = useFetchCoworkDetails(coworkId)
-
-  //  TODO: Handle status notifications
+  // TODO: Handle status notifications
+  // TODO: Implement skeleton loader
+  const { coworkDetails, isLoading } = useFetchCoworkDetails(coworkId)
 
   return (
     <div className="m-8 flex min-h-[calc(100vh-8.1rem)] w-screen flex-col gap-2 md:gap-6">
-      <h1 className="text-center text-6xl font-bold">{coworkDetails?.name}</h1>
-      {!isFetched && <p>Could not fetch the data</p>}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <div>
+            <h1 className="text-center text-6xl font-bold">
+              {coworkDetails?.name}
+            </h1>
+            <p className="mt-4 text-center">{coworkDetails?.description}</p>
+            {/* Show status with color badge? */}
+            <p className="font-bold">Status: {coworkDetails?.status}</p>
+            {coworkDetails?.image && (
+              <Image
+                src={coworkDetails?.image}
+                width={500}
+                height={500}
+                alt="Cowork image"
+              />
+            )}
+          </div>
+          <div className="my-4">
+            <p className="font-bold">Address</p>
+            <p>Country:{coworkDetails?.address.country}</p>
+            <p>City: {coworkDetails?.address.city}</p>
 
-      <p className="mt-4 text-center">{coworkDetails?.description}</p>
-      <div className="my-4">
-        <p>Email: {coworkDetails?.email}</p>
-        <p>Phone: {coworkDetails?.phone}</p>
+            <p className="font-bold">Amenities</p>
+            <p>
+              {coworkDetails?.amenities?.bathrooms &&
+                `✅ has ${coworkDetails?.amenities?.bathrooms} bathrooms`}
+            </p>
+            <p>{coworkDetails?.amenities?.buffet && '✅ Buffet'}</p>
+            <p>{coworkDetails?.amenities?.wifi ? '✅ Wifi' : 'No Wifi'}</p>
 
-        <p>Amenities</p>
-        <p>{coworkDetails?.amenities?.bathrooms}</p>
-        <p>{coworkDetails?.email}</p>
-
-        <p>Address</p>
-        <p>Country:{coworkDetails?.address.country}</p>
-        <p>City: {coworkDetails?.address.city}</p>
-      </div>
+            <p className="font-bold">Open schedule</p>
+            <p>Mon: {coworkDetails?.openSchedule?.mon}</p>
+            <p>Tue: {coworkDetails?.openSchedule?.tue}</p>
+            <p>Wed: {coworkDetails?.openSchedule?.wed}</p>
+            <p>Thu: {coworkDetails?.openSchedule?.thu}</p>
+            <p>Fri: {coworkDetails?.openSchedule?.fri}</p>
+            <p>Sat: {coworkDetails?.openSchedule?.sat}</p>
+            <p>Sun: {coworkDetails?.openSchedule?.sun}</p>
+          </div>
+        </>
+      )}
     </div>
   )
 }
