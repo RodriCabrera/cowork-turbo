1849133ed8db43fc1aa7afd6d0d30a258865e09f
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-07-17 10:44:53 -0300

    style: add basic styling

--- apps/front/modules/contact/ContactForm.tsx
+++ apps/front/modules/contact/ContactForm.tsx
@@ -17,7 +17,6 @@ const inputSchema = z.object({
   })
 })
 
-// TODO: styles
 export const ContactForm: FC = () => {
   const {
     register,
@@ -30,45 +29,73 @@ export const ContactForm: FC = () => {
   // TODO: implement
   const onSubmit = () => null
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="email">
-        <p>Email</p>
-        <input
-          id="email"
-          type="email"
-          {...register('from.email', { required: true })}
-        />
-        <FormError error={errors.from?.email} />
-      </label>
-      <label htmlFor="name">
-        <p>Name</p>
-        <input id="name" {...register('from.name', { required: true })} />
-        <FormError error={errors.from?.name} />
-      </label>
-
-      <label htmlFor="company">
-        <p>Company</p>
-        <input id="company" {...register('from.companyName')} />
-        <FormError error={errors.from?.companyName} />
-      </label>
-
-      <label htmlFor="phone">
-        <p>Phone</p>
-        <input id="phone" type="tel" {...register('from.phone')} />
-        <FormError error={errors.from?.phone} />
-      </label>
-
-      <label htmlFor="country">
-        <p>Country</p>
-        <input id="country" {...register('from.country')} />
-        <FormError error={errors.from?.country} />
-      </label>
-
-      <label htmlFor="message">
-        <p>Message</p>
-        <textarea id="message" {...register('message')} />
-        <FormError error={errors.message} />
-      </label>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex w-full flex-col justify-center sm:w-1/2"
+    >
+      <div className="m-auto my-4 flex w-full justify-center gap-6">
+        <div className="w-1/3">
+          <label htmlFor="email">
+            <p className="py-2">Email</p>
+            <input
+              id="email"
+              type="email"
+              {...register('from.email', { required: true })}
+              className="w-full p-2"
+            />
+            <FormError error={errors.from?.email} />
+          </label>
+          <label htmlFor="name">
+            <p className="py-2">Name</p>
+            <input
+              id="name"
+              {...register('from.name', { required: true })}
+              className="w-full p-2"
+            />
+            <FormError error={errors.from?.name} />
+          </label>
+          <label htmlFor="company">
+            <p className="py-2">Company</p>
+            <input
+              id="company"
+              {...register('from.companyName')}
+              className="w-full p-2"
+            />
+            <FormError error={errors.from?.companyName} />
+          </label>
+          <label htmlFor="phone">
+            <p className="py-2">Phone</p>
+            <input
+              id="phone"
+              type="tel"
+              {...register('from.phone')}
+              className="w-full p-2"
+            />
+            <FormError error={errors.from?.phone} />
+          </label>
+          <label htmlFor="country">
+            <p className="py-2">Country</p>
+            <input
+              id="country"
+              {...register('from.country')}
+              className="w-full p-2"
+            />
+            <FormError error={errors.from?.country} />
+          </label>
+        </div>
+        <label htmlFor="message" className="grow">
+          <p className="py-2">Message</p>
+          <textarea
+            id="message"
+            {...register('message')}
+            className="h-[calc(100%-2.5rem)] w-full resize-none p-2"
+          />
+          <FormError error={errors.message} />
+        </label>
+      </div>
+      <button type="submit" className="my-1 w-full bg-green-300 py-2">
+        Send
+      </button>
     </form>
   )
 }
--- apps/front/pages/contact.tsx
+++ apps/front/pages/contact.tsx
@@ -7,7 +7,7 @@ import { ContactForm } from '@/modules/contact/ContactForm'
 
 export const ContactPage = ({ user }: PropsWithUser) => (
   <BaseLayout user={user}>
-    <div className="flex min-h-[calc(100vh-126px)] justify-center bg-gray-50 pb-20 md:p-0">
+    <div className="flex min-h-[calc(100vh-126px)] flex-col items-center justify-center bg-gray-50 pb-20 md:p-0">
       <ContactBanner />
       <ContactForm />
     </div>
