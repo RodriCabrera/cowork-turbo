d815d2dc24f7c5386ee66d0747ae478a46500608
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-07-18 14:55:38 -0300

    refactor html lists with map

--- apps/front/modules/coworks/components/CoworkDetails/index.tsx
+++ apps/front/modules/coworks/components/CoworkDetails/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 import { useFetchCoworkDetails } from '../../hooks/useFetchCoworkDetails'
 import { COLORS_BY_STATUS } from '../CoworksTable/constants'
+import { LoaderThreeDots } from '@/common/components/LoaderThreeDots'
 
 export const CoworkDetails = ({ coworkId }: { coworkId: string }) => {
   // TODO: Handle status notifications
@@ -11,7 +12,7 @@ export const CoworkDetails = ({ coworkId }: { coworkId: string }) => {
   return (
     <div className="m-8 flex flex-col items-center gap-2 md:gap-6">
       {isLoading ? (
-        <p>Loading...</p>
+        <LoaderThreeDots />
       ) : (
         <div className="md:w-3/4">
           <header>
@@ -44,24 +45,51 @@ export const CoworkDetails = ({ coworkId }: { coworkId: string }) => {
                 alt="Cowork image"
               />
             )}
-          </header>{' '}
-          <div className="flex gap-4">
-            <p>{coworkDetails?.amenities?.bathrooms && '🚻 Bathroom'}</p>
-            <p>{coworkDetails?.amenities?.buffet && '✅ Buffet'}</p>
-            <p>{coworkDetails?.amenities?.wifi ? '✅ Wifi' : 'No Wifi'}</p>
-          </div>
+          </header>
+          {/* DESCRIPTION */}
           <p className="mt-4">{coworkDetails?.description}</p>
+          {/* AMENITIES */}
+          <div className="flex gap-4">
+            {[
+              {
+                id: 'bathroom',
+                text: '🚻 Bathroom',
+                accessor: coworkDetails?.amenities?.bathrooms
+              },
+              {
+                id: 'buffet',
+                text: '✅ Buffet',
+                accessor: coworkDetails?.amenities?.buffet
+              },
+              {
+                id: 'wifi',
+                text: '✅ Wifi',
+                accessor: coworkDetails?.amenities?.wifi
+              }
+            ].map((amenity) => {
+              if (!amenity.accessor) return null
+              return <p key={amenity.id}>{amenity.text}</p>
+            })}
+          </div>
+          {/* SCHEDULE */}
           <div className="my-4">
-            <p className="font-bold">Amenities</p>
-
             <p className="font-bold">Open schedule</p>
-            <p>Mon: {coworkDetails?.openSchedule?.mon}</p>
-            <p>Tue: {coworkDetails?.openSchedule?.tue}</p>
-            <p>Wed: {coworkDetails?.openSchedule?.wed}</p>
-            <p>Thu: {coworkDetails?.openSchedule?.thu}</p>
-            <p>Fri: {coworkDetails?.openSchedule?.fri}</p>
-            <p>Sat: {coworkDetails?.openSchedule?.sat}</p>
-            <p>Sun: {coworkDetails?.openSchedule?.sun}</p>
+            {[
+              { label: 'Mon', value: coworkDetails?.openSchedule?.mon },
+              { label: 'Tue', value: coworkDetails?.openSchedule?.tue },
+              { label: 'Wed', value: coworkDetails?.openSchedule?.wed },
+              { label: 'Thu', value: coworkDetails?.openSchedule?.thu },
+              { label: 'Fri', value: coworkDetails?.openSchedule?.fri },
+              { label: 'Sat', value: coworkDetails?.openSchedule?.sat },
+              { label: 'Sun', value: coworkDetails?.openSchedule?.sun }
+            ].map((day) => {
+              if (!day.value) return null
+              return (
+                <p key={day.label}>
+                  {day.label}: {day.value}
+                </p>
+              )
+            })}
           </div>
         </div>
       )}
